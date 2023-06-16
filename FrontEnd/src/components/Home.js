d1123dc2ccb2798c117e254add3c48b4f8acd95f
Vincent.yu <gn870988@gmail.com>
2023-06-16 17:20:38 +0800

    End of section 16

---
+++ FrontEnd/src/components/Home.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from "react";
+import MetaData from "./layout/MetaData";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <MetaData title={"The best products online"} />
+      <section id="products" className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div className="card p-3 rounded">
+              <img
+                className="card-img-top mx-auto"
+                src="https://firebasestorage.googleapis.com/v0/b/ecommerce-e4e6c.appspot.com/o/images%2Fzapato8.jpg-1617998795201?alt=media&token=9e62c323-1a92-4be4-8507-34cee836d5ea"
+                alt="test"
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">
+                  <a href="https://www.google.com.tw/">
+                    128GB Solid Storage Memory card - SanDisk Ultra
+                  </a>
+                </h5>
+                <div className="ratings mt-auto">
+                  <div className="rating-outer">
+                    <div className="rating-inner"></div>
+                  </div>
+                  <span id="no_of_reviews">(5 Reviews)</span>
+                </div>
+                <p className="card-text">$45.67</p>
+                <a
+                  href="https://www.google.com.tw/"
+                  id="view_btn"
+                  className="btn btn-block"
+                >
+                  Ver Detalles
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Home;
